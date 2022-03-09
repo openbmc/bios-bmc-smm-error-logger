d1cf85b39c028db9126d6ca0edec39932a77c2f8
Brandon Kim <brandonkim@google.com>
2022-03-09 12:32:36 -0800

    pci_handler: Add pci_handler
    Taken most of the logic from phoshpor-ipmi-flash:
    https://github.com/openbmc/phosphor-ipmi-flash/blob/master/tools/io.cpp
    
    Tested: Unit Test
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: I546d1dff8764e31c9f2fa6c7465ba23a24f6fdea

--- internal/sys.hpp
+++ internal/sys.hpp
@@ -17,11 +17,11 @@ class Sys
     virtual ~Sys() = default;
 
     virtual int open(const char* pathname, int flags) const = 0;
-    virtual int read(int fd, void* buf, std::size_t count) const = 0;
+    virtual int read(int fd, void* buf, size_t count) const = 0;
     virtual int close(int fd) const = 0;
-    virtual void* mmap(void* addr, std::size_t length, int prot, int flags,
-                       int fd, off_t offset) const = 0;
-    virtual int munmap(void* addr, std::size_t length) const = 0;
+    virtual void* mmap(void* addr, size_t length, int prot, int flags, int fd,
+                       off_t offset) const = 0;
+    virtual int munmap(void* addr, size_t length) const = 0;
     virtual int ioctl(int fd, unsigned long request, void* param) const = 0;
 };
 
@@ -34,12 +34,15 @@ class SysImpl : public Sys
 {
   public:
     int open(const char* pathname, int flags) const override;
-    int read(int fd, void* buf, std::size_t count) const override;
+    int read(int fd, void* buf, size_t count) const override;
     int close(int fd) const override;
-    void* mmap(void* addr, std::size_t length, int prot, int flags, int fd,
+    void* mmap(void* addr, size_t length, int prot, int flags, int fd,
                off_t offset) const override;
-    int munmap(void* addr, std::size_t length) const override;
+    int munmap(void* addr, size_t length) const override;
     int ioctl(int fd, unsigned long request, void* param) const override;
 };
+
+/** @brief Default instantiation of sys */
+extern SysImpl sys_impl;
 
 } // namespace internal
