a80a49ee053df3347202dce406201f5f40165ebe
Brandon Kim <brandonkim@google.com>
2022-03-09 02:18:01 -0800

    pci_handler: Add pci_handler
    Taken most of the logic from phoshpor-ipmi-flash:
    https://github.com/openbmc/phosphor-ipmi-flash/blob/master/tools/io.cpp
    
    Tested: Unit Test + Tested this on a real machine
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: I546d1dff8764e31c9f2fa6c7465ba23a24f6fdea

---
+++ src/pci_handler.cpp
@@ -0,0 +1,73 @@
+#include "pci_handler.hpp"
+
+#include <fcntl.h>
+#include <fmt/format.h>
+
+#include <stdplus/fd/managed.hpp>
+#include <stdplus/fd/mmap.hpp>
+
+#include <cstdint>
+#include <cstring>
+#include <memory>
+#include <span>
+#include <vector>
+
+namespace biosBmcSmmErrorLogger
+{
+
+PciDataHandler::PciDataHandler(uint32_t regionAddress, size_t regionSize,
+                               std::unique_ptr<stdplus::fd::Fd> fd) :
+    regionAddress(regionAddress),
+    regionSize(regionSize), fd(std::move(fd)),
+    mmap(stdplus::fd::MMap(
+        *this->fd, regionSize, stdplus::fd::ProtFlags{PROT_READ | PROT_WRITE},
+        stdplus::fd::MMapFlags{stdplus::fd::MMapAccess::Shared}, regionAddress))
+{}
+
+std::vector<uint8_t> PciDataHandler::read(const uint32_t offset,
+                                          const uint32_t length)
+{
+    if (offset > regionSize || length == 0)
+    {
+        fmt::print(stderr,
+                   "[read] Offset [{}] was bigger than regionSize [{}] "
+                   "OR length [{}] was equal to 0\n",
+                   offset, regionSize, length);
+        return {};
+    }
+
+    // Read up to regionSize in case the offset + length overflowed
+    uint32_t finalLength =
+        (offset + length < regionSize) ? length : regionSize - offset;
+    std::vector<uint8_t> results(finalLength);
+
+    std::memcpy(results.data(), mmap.get().data() + offset, finalLength);
+    return results;
+}
+
+uint32_t PciDataHandler::write(const uint32_t offset,
+                               const std::span<const uint8_t> bytes)
+{
+    const size_t length = bytes.size();
+    if (offset > regionSize || length == 0)
+    {
+        fmt::print(stderr,
+                   "[write] Offset [{}] was bigger than regionSize [{}] "
+                   "OR length [{}] was equal to 0\n",
+                   offset, regionSize, length);
+        return {};
+    }
+
+    // Write up to regionSize in case the offset + length overflowed
+    uint16_t finalLength =
+        (offset + length < regionSize) ? length : regionSize - offset;
+    std::memcpy(mmap.get().data() + offset, bytes.data(), finalLength);
+    return finalLength;
+}
+
+uint32_t PciDataHandler::getMemoryRegionSize()
+{
+    return regionSize;
+}
+
+} // namespace biosBmcSmmErrorLogger
