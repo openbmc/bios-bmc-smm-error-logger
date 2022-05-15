c6e2325875acc377d75e9eb19b38cceaa18d93eb
Brandon Kim <brandonkim@google.com>
2022-05-15 15:41:05 -0700

    Add basic main and meson.build
    Main just runs very 1 second, based on the asio timer example.
    
    Tested: Added a debug fprint and verified that it runs every 1 second
    
    Signed-off-by: Brandon Kim <brandonkim@google.com>
    Change-Id: I75c98a39f43d3f7103583171979ce87515c9083d

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+#include <chrono>
+#include <functional>
+
+namespace
+{
+// Set the read loop interval to be 1 second for now
+// TODO: Make this a configuration option
+static constexpr std::chrono::seconds readIntervalSec(1);
+} // namespace
+
+// boost::async_wait requires `const boost::system::error_code&` parameter
+void readLoop(boost::asio::steady_timer* t, const boost::system::error_code&)
+{
+    /* This will run every readIntervalSec second for now */
+    t->expires_from_now(readIntervalSec);
+    t->async_wait(std::bind_front(readLoop, t));
+}
+
+int main()
+{
+    boost::asio::io_context io;
+    boost::asio::steady_timer t(io,
+                                boost::asio::chrono::seconds(readIntervalSec));
+
+    t.async_wait(std::bind_front(readLoop, &t));
+
+    io.run();
+
+    return 0;
+}
