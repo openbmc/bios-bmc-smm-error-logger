8f6d1531a4c2bd4f81c3242d3e1c6c5cb67feb46
kasunath <kasunath@google.com>
2022-05-27 14:31:38 -0700

    Add a class to handle RDE BEJ dictionary data
    This class is used to store RDE BEJ dictionary data transmitted
    through bios-bmc circular buffer interface.
    
    Signed-off-by: Kasun Athukorala <kasunath@google.com>
    Change-Id: Idf7726a9f4647885ede615229d507f233ffb13c5

---
+++ include/rde_dictionary_manager.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <memory>
+#include <optional>
+#include <span>
+#include <unordered_map>
+#include <vector>
+
+namespace biosBmcSmmErrorLogger
+{
+namespace rde
+{
+
+/**
+ * @brief Resource ID for the annotation dictionary. The other entity
+ * communicating with the BMC (Eg: BIOS) should use the same resource ID for the
+ * annotation dictionary.
+ */
+constexpr uint32_t annotationResourceId = 0;
+
+/**
+ * @brief Holds a RDE BEJ dictionary entry.
+ */
+struct DictionaryEntry
+{
+    DictionaryEntry(bool valid, std::span<const uint8_t> data) :
+        valid(valid), data(data.begin(), data.end())
+    {}
+    // True indicates that the dictionary data is ready to be used.
+    bool valid;
+    std::vector<uint8_t> data;
+};
+
+/**
+ * @brief Manages RDE BEJ dictionaries.
+ */
+class DictionaryManager
+{
+  public:
+    DictionaryManager();
+
+    /**
+     * @brief Starts a dictionary entry with the provided data.
+     *
+     * @param resourceId - PDR resource id corresponding to the dictionary.
+     * @param data - dictionary data.
+     */
+    void startDictionaryEntry(uint32_t resourceId,
+                              std::span<const uint8_t> data);
+
+    /**
+     * @brief Set the dictionary valid status. Until this is called, dictionary
+     * data is considered to be incomplete for use.
+     *
+     * @param resourceId - PDR resource id corresponding to the dictionary.
+     * @return true if successful.
+     */
+    bool markDataComplete(uint32_t resourceId);
+
+    /**
+     * @brief Add more dictionary data for an existing entry. Adding data to a
+     * completed dictionary will mark the dictionary as incomplete.
+     *
+     * @param resourceId - PDR resource id corresponding to the dictionary.
+     * @param data - dictionary data.
+     * @return true if successful.
+     */
+    bool addMoreDictionaryData(uint32_t resourceId,
+                               std::span<const uint8_t> data);
+
+    /**
+     * @brief Get a dictionary.
+     *
+     * @param resourceId - PDR resource id corresponding to the dictionary.
+     * @return a pointer to the dictionary, if the dictionary is complete else
+     * std::nullopt.
+     */
+    std::optional<std::span<const uint8_t>> getDictionary(uint32_t resourceId);
+
+    /**
+     * @brief Get the annotation dictionary.
+     *
+     * @return a pointer to the annotation dictionary, if the dictionary is
+     * complete else std::nullopt.
+     */
+    std::optional<std::span<const uint8_t>> getAnnotationDictionary();
+
+    /**
+     * @brief Get the completed dictionary count.
+     *
+     * @return number of completed dictionaries available.
+     */
+    uint32_t getDictionaryCount();
+
+    /**
+     * @brief Invalidate all dictionaries.
+     */
+    void invalidateDictionaries();
+
+  private:
+    uint32_t dictionaryCount;
+    std::unordered_map<uint32_t, std::unique_ptr<DictionaryEntry>> dictionaries;
+};
+
+} // namespace rde
+} // namespace biosBmcSmmErrorLogger
---
+++ src/rde_dictionary_manager.cpp
@@ -0,0 +1,108 @@
+#include "rde_dictionary_manager.hpp"
+
+#include <fmt/format.h>
+
+namespace biosBmcSmmErrorLogger
+{
+namespace rde
+{
+
+DictionaryManager::DictionaryManager() : dictionaryCount(0)
+{}
+
+void DictionaryManager::startDictionaryEntry(uint32_t resourceId,
+                                             std::span<const uint8_t> data)
+{
+    // Check whether the resourceId is already available.
+    auto itemIt = dictionaries.find(resourceId);
+    if (itemIt == dictionaries.end())
+    {
+        dictionaries[resourceId] =
+            std::make_unique<DictionaryEntry>(false, data);
+        return;
+    }
+    // Reduce the dictionary count since we are creating a new dictionary on a
+    // existing entry.
+    if (itemIt->second->valid)
+    {
+        --dictionaryCount;
+    }
+    itemIt->second->valid = false;
+    // Flush the existing data.
+    itemIt->second->data.clear();
+    itemIt->second->data.insert(itemIt->second->data.begin(), data.begin(),
+                                data.end());
+}
+
+bool DictionaryManager::markDataComplete(uint32_t resourceId)
+{
+    auto itemIt = dictionaries.find(resourceId);
+    if (itemIt == dictionaries.end())
+    {
+        fmt::print(stderr, "Resource ID {} not found.\n", resourceId);
+        return false;
+    }
+    itemIt->second->valid = true;
+    ++dictionaryCount;
+    return true;
+}
+
+bool DictionaryManager::addMoreDictionaryData(uint32_t resourceId,
+                                              std::span<const uint8_t> data)
+{
+    auto itemIt = dictionaries.find(resourceId);
+    if (itemIt == dictionaries.end())
+    {
+        fmt::print(stderr, "Resource ID {} not found.\n", resourceId);
+        return false;
+    }
+    itemIt->second->valid = false;
+    itemIt->second->data.insert(itemIt->second->data.end(), data.begin(),
+                                data.end());
+    return true;
+}
+
+std::optional<std::span<const uint8_t>>
+    DictionaryManager::getDictionary(uint32_t resourceId)
+{
+    auto itemIt = dictionaries.find(resourceId);
+    if (itemIt == dictionaries.end())
+    {
+        fmt::print(stderr, "Resource ID {} not found.\n", resourceId);
+        return std::nullopt;
+    }
+
+    if (!itemIt->second->valid)
+    {
+        fmt::print(stderr,
+                   "Requested an incomplete dictionary. Resource ID {}\n",
+                   resourceId);
+        return std::nullopt;
+    }
+    return std::span(itemIt->second->data);
+}
+
+std::optional<std::span<const uint8_t>>
+    DictionaryManager::getAnnotationDictionary()
+{
+    return getDictionary(annotationResourceId);
+}
+
+uint32_t DictionaryManager::getDictionaryCount()
+{
+    return dictionaryCount;
+}
+
+void DictionaryManager::invalidateDictionaries()
+{
+    // We won't flush the existing data. The data will be flushed if a new entry
+    // is added for an existing resource ID.
+    for (auto& element : dictionaries)
+    {
+        element.second->valid = false;
+    }
+    dictionaryCount = 0;
+}
+
+} // namespace rde
+} // namespace biosBmcSmmErrorLogger
