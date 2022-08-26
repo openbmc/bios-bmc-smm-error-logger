e97f9268e54762f5f54fe5d3bbde8ba7f2d45aa8
kasunath <kasunath@google.com>
2022-08-25 20:22:58 -0700

    Modify D-Bus to using asio server
    DBus updates are now using the asio server way.
    
    Tested:
    Tested with unit tests and locally on a machine.
    
    Signed-off-by: Kasun Athukorala <kasunath@google.com>
    Change-Id: I77553bcc3baae70e5d684a62f2c19592ff844665
    Signed-off-by: Brandon Kim <brandonkim@google.com>

--- include/dbus/file_notifier.hpp
+++ include/dbus/file_notifier.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <sdbusplus/server.hpp>
+#include <fmt/format.h>
+
+#include <sdbusplus/asio/object_server.hpp>
 #include <xyz/openbmc_project/Common/FilePath/server.hpp>
 
 #include <format>
@@ -9,36 +11,58 @@
 namespace bios_bmc_smm_error_logger
 {
 
-using FileNotifierInterface = sdbusplus::server::object_t<
-    sdbusplus::xyz::openbmc_project::Common::server::FilePath>;
-
 /**
  * @brief A class for notifying file paths of CPER logs.
  */
-class CperFileNotifier : public FileNotifierInterface
+class CperFileNotifier
 {
   public:
     /**
      * @brief Constructor for the CperFileNotifier class.
      *
-     * @param bus - bus to attach to.
+     * @param server - sdbusplus asio object server.
      * @param filePath - full path of the CPER log JSON file.
      * @param entry - index of the DBus file path object.
      */
-    CperFileNotifier(sdbusplus::bus_t& bus, const std::string& filePath,
-                     uint64_t entry) :
-        FileNotifierInterface(bus, generatePath(entry).c_str(),
-                              action::emit_no_signals)
+    CperFileNotifier(sdbusplus::asio::object_server& server,
+                     const std::string& filePath, uint64_t entry) :
+        server(server)
     {
-        // We only need the interface added signal for the fault monitor. So
-        // stop emitting properties changed signal.
-        path(filePath, /*skipSignal=*/true);
+        // More Logging.Entry fields can be populated if it's required. For the
+        // moment only severity field is updated. This interface is mainly
+        // added so that we can use FilePath interface alongside with this.
+        const std::string& severity =
+            "xyz.openbmc_project.Logging.Entry.Level.Informational";
+        logEntryIface = server.add_interface(
+            generatePath(entry).c_str(), "xyz.openbmc_project.Logging.Entry");
+        logEntryIface->register_property("Severity", severity);
+        logEntryIface->initialize();
+
+        pathIface = server.add_interface(generatePath(entry).c_str(),
+                                         "xyz.openbmc_project.Common.FilePath");
+        pathIface->register_property("Path", filePath);
+        pathIface->initialize();
     }
+
+    ~CperFileNotifier()
+    {
+        server.remove_interface(pathIface);
+        server.remove_interface(logEntryIface);
+    }
+
+    CperFileNotifier& operator=(const CperFileNotifier&) = delete;
+    CperFileNotifier& operator=(CperFileNotifier&&) = delete;
+    CperFileNotifier(const CperFileNotifier&) = delete;
+    CperFileNotifier(CperFileNotifier&&) = default;
 
     static constexpr const char* cperBasePath =
         "/xyz/openbmc_project/external_storer/bios_bmc_smm_error_logger/CPER";
 
   private:
+    sdbusplus::asio::object_server& server;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> logEntryIface;
+    std::shared_ptr<sdbusplus::asio::dbus_interface> pathIface;
+
     /**
      * @brief Generate a path for the CperFileNotifier DBus object.
      *
