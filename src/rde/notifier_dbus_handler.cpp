092d2a95d845f22b7b46392fec339d2f1c6501de
Hao Zhou <haoooamazing@google.com>
2024-08-31 02:30:30 +0000

    external_storer: fix asan on presubmit
    Tested: TBD
    Change-Id: Icfbbf62744a1d4a39d568b843a6624581134a3b8
    Signed-off-by: Hao Zhou <haoooamazing@google.com>

--- src/rde/notifier_dbus_handler.cpp
+++ src/rde/notifier_dbus_handler.cpp
@@ -13,8 +13,8 @@ void CperFileNotifierHandler::createEntry(const std::string& filePath)
 {
     auto obj = std::make_unique<CperFileNotifier>(bus, filePath, nextEntry);
     // Notify fault log monitor through InterfacesAdded signal.
-    obj->emit_added();
-    notifierObjs.push_back(std::move(obj));
+    // obj->emit_added();
+    // notifierObjs.push_back(std::move(obj));
     ++nextEntry;
 }
 
