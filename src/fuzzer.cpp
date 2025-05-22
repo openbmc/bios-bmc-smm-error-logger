f12b2f36d103d954814799d4848791f97dd61cde
Javan Lacerda <javanlacerda@google.com>
2025-05-22 19:34:59 +0000

    fix lint
    Change-Id: I8604a6f79cb90d02715744f9475bde9ef5a16057
    Signed-off-by: Javan Lacerda <javanlacerda@google.com>

--- src/fuzzer.cpp
+++ src/fuzzer.cpp
@@ -26,9 +26,10 @@ constexpr std::size_t memoryRegionSize = MEMORY_REGION_SIZE;
 constexpr uint32_t bmcInterfaceVersion = BMC_INTERFACE_VERSION;
 constexpr uint16_t queueSize = QUEUE_REGION_SIZE;
 constexpr uint16_t ueRegionSize = UE_REGION_SIZE;
-static constexpr std::array<uint32_t, 4> magicNumber = {
-	MAGIC_NUMBER_BYTE1, MAGIC_NUMBER_BYTE2, MAGIC_NUMBER_BYTE3,
-	MAGIC_NUMBER_BYTE4
+static constexpr std::array<uint32_t, 4> magicNumber =
+{
+    MAGIC_NUMBER_BYTE1, MAGIC_NUMBER_BYTE2, MAGIC_NUMBER_BYTE3,
+    MAGIC_NUMBER_BYTE4
 };
 
 // Global state variables
@@ -38,51 +39,57 @@ static std::shared_ptr<BufferInterface> bufferHandler;
 static std::shared_ptr<rde::RdeCommandHandler> rdeCommandHandler;
 } // namespace
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* Data, size_t Size)
 {
-	if (!isInitialized) {
-		buffer.resize(memoryRegionSize);
+    if (!isInitialized)
+    {
+        buffer.resize(memoryRegionSize);
 
-		boost::asio::io_context io;
-		boost::asio::steady_timer t(io, readIntervalinMs); // ignored for fuzzing
+        boost::asio::io_context io;
+        boost::asio::steady_timer t(io, readIntervalinMs);    // ignored for fuzzing
 
-		// bufferHandler initialization
-		std::unique_ptr<DataInterface> pciDataHandler =
-			std::make_unique<PciDataHandler>(buffer.data(), memoryRegionSize);
-		bufferHandler = std::make_shared<BufferImpl>(std::move(pciDataHandler));
+        // bufferHandler initialization
+        std::unique_ptr<DataInterface> pciDataHandler =
+            std::make_unique<PciDataHandler> (buffer.data(), memoryRegionSize);
+        bufferHandler = std::make_shared<BufferImpl> (std::move(pciDataHandler));
 
-		// rdeCommandHandler initialization
-		std::shared_ptr<sdbusplus::asio::connection> conn =
-			std::make_shared<sdbusplus::asio::connection>(io);
-		conn->request_name("xyz.openbmc_project.bios_bmc_smm_error_logger");
+        // rdeCommandHandler initialization
+        std::shared_ptr<sdbusplus::asio::connection> conn =
+            std::make_shared<sdbusplus::asio::connection> (io);
+        conn->request_name("xyz.openbmc_project.bios_bmc_smm_error_logger");
 
-		std::unique_ptr<rde::FileHandlerInterface> fileIface =
-			std::make_unique<rde::ExternalStorerFileWriter>();
-		std::unique_ptr<rde::ExternalStorerInterface> exFileIface =
-			std::make_unique<rde::ExternalStorerFileInterface>(
-				conn, "/run/bmcweb", std::move(fileIface));
+        std::unique_ptr<rde::FileHandlerInterface> fileIface =
+            std::make_unique<rde::ExternalStorerFileWriter>();
+        std::unique_ptr<rde::ExternalStorerInterface> exFileIface =
+            std::make_unique<rde::ExternalStorerFileInterface> (
+                conn, "/run/bmcweb", std::move(fileIface));
 
-		rdeCommandHandler =
-			std::make_unique<rde::RdeCommandHandler>(std::move(exFileIface));
+        rdeCommandHandler =
+            std::make_unique<rde::RdeCommandHandler> (std::move(exFileIface));
 
-		bufferHandler->initialize(bmcInterfaceVersion, queueSize, ueRegionSize,
-					  magicNumber);
-		isInitialized = true;
-	}
+        bufferHandler->initialize(bmcInterfaceVersion, queueSize, ueRegionSize,
+                                  magicNumber);
+        isInitialized = true;
+    }
 
-	std::fill(buffer.begin(), buffer.end(), 0);
-	std::copy(Data, Data + std::min(Size, buffer.size()), buffer.begin());
+    std::fill(buffer.begin(), buffer.end(), 0);
+    std::copy(Data, Data + std::min(Size, buffer.size()), buffer.begin());
 
-	try {
-		boost::system::error_code ec;
-		bios_bmc_smm_error_logger::readLoop(
-			static_cast<boost::asio::basic_waitable_timer<std::chrono::_V2::steady_clock>*>(nullptr),
-			bufferHandler,
-			rdeCommandHandler,
-			ec
-		);
-	} catch (const std::runtime_error& e) {
-		std::cout << "Caught std::runtime_error (ignored by fuzzer): " << e.what() << std::endl;
-	}
-	return 0;
+    try
+    {
+        boost::system::error_code ec;
+        bios_bmc_smm_error_logger::readLoop(
+            static_cast<boost::asio::basic_waitable_timer<std::chrono::_V2::steady_clock>*>
+            (nullptr),
+            bufferHandler,
+            rdeCommandHandler,
+            ec
+        );
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cout << "Caught std::runtime_error (ignored by fuzzer): " << e.what() <<
+                  std::endl;
+    }
+    return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,10 @@ constexpr std::size_t memoryRegionOffset = MEMORY_REGION_OFFSET;
 constexpr uint32_t bmcInterfaceVersion = BMC_INTERFACE_VERSION;
 constexpr uint16_t queueSize = QUEUE_REGION_SIZE;
 constexpr uint16_t ueRegionSize = UE_REGION_SIZE;
-static constexpr std::array<uint32_t, 4> magicNumber = {
-	MAGIC_NUMBER_BYTE1, MAGIC_NUMBER_BYTE2, MAGIC_NUMBER_BYTE3,
-	MAGIC_NUMBER_BYTE4
+static constexpr std::array<uint32_t, 4> magicNumber =
+{
+    MAGIC_NUMBER_BYTE1, MAGIC_NUMBER_BYTE2, MAGIC_NUMBER_BYTE3,
+    MAGIC_NUMBER_BYTE4
 };
 } // namespace
 
@@ -34,40 +35,40 @@ using namespace bios_bmc_smm_error_logger;
 
 int main()
 {
-	boost::asio::io_context io;
-	boost::asio::steady_timer t(io, readIntervalinMs);
+    boost::asio::io_context io;
+    boost::asio::steady_timer t(io, readIntervalinMs);
 
-	// bufferHandler initialization
-	std::unique_ptr<stdplus::ManagedFd> managedFd =
-		std::make_unique<stdplus::ManagedFd>(stdplus::fd::open(
-				"/dev/mem",
-				stdplus::fd::OpenFlags(stdplus::fd::OpenAccess::ReadWrite)
-				.set(stdplus::fd::OpenFlag::Sync)));
-	std::unique_ptr<DataInterface> pciDataHandler =
-		std::make_unique<PciDataHandler>(memoryRegionOffset, memoryRegionSize,
-			std::move(managedFd));
-	std::shared_ptr<BufferInterface> bufferHandler =
-		std::make_shared<BufferImpl>(std::move(pciDataHandler));
+    // bufferHandler initialization
+    std::unique_ptr<stdplus::ManagedFd> managedFd =
+        std::make_unique<stdplus::ManagedFd> (stdplus::fd::open(
+                "/dev/mem",
+                stdplus::fd::OpenFlags(stdplus::fd::OpenAccess::ReadWrite)
+                .set(stdplus::fd::OpenFlag::Sync)));
+    std::unique_ptr<DataInterface> pciDataHandler =
+        std::make_unique<PciDataHandler> (memoryRegionOffset, memoryRegionSize,
+                                          std::move(managedFd));
+    std::shared_ptr<BufferInterface> bufferHandler =
+        std::make_shared<BufferImpl> (std::move(pciDataHandler));
 
-	// rdeCommandHandler initialization
-	std::shared_ptr<sdbusplus::asio::connection> conn =
-		std::make_shared<sdbusplus::asio::connection>(io);
-	conn->request_name("xyz.openbmc_project.bios_bmc_smm_error_logger");
+    // rdeCommandHandler initialization
+    std::shared_ptr<sdbusplus::asio::connection> conn =
+        std::make_shared<sdbusplus::asio::connection> (io);
+    conn->request_name("xyz.openbmc_project.bios_bmc_smm_error_logger");
 
-	std::unique_ptr<rde::FileHandlerInterface> fileIface =
-		std::make_unique<rde::ExternalStorerFileWriter>();
-	std::unique_ptr<rde::ExternalStorerInterface> exFileIface =
-		std::make_unique<rde::ExternalStorerFileInterface>(
-			conn, "/run/bmcweb", std::move(fileIface));
-	std::shared_ptr<rde::RdeCommandHandler> rdeCommandHandler =
-		std::make_unique<rde::RdeCommandHandler>(std::move(exFileIface));
+    std::unique_ptr<rde::FileHandlerInterface> fileIface =
+        std::make_unique<rde::ExternalStorerFileWriter>();
+    std::unique_ptr<rde::ExternalStorerInterface> exFileIface =
+        std::make_unique<rde::ExternalStorerFileInterface> (
+            conn, "/run/bmcweb", std::move(fileIface));
+    std::shared_ptr<rde::RdeCommandHandler> rdeCommandHandler =
+        std::make_unique<rde::RdeCommandHandler> (std::move(exFileIface));
 
-	bufferHandler->initialize(bmcInterfaceVersion, queueSize, ueRegionSize,
-				  magicNumber);
+    bufferHandler->initialize(bmcInterfaceVersion, queueSize, ueRegionSize,
+                              magicNumber);
 
-	t.async_wait(std::bind_front(readLoop, &t, std::move(bufferHandler),
-				     std::move(rdeCommandHandler)));
-	io.run();
+    t.async_wait(std::bind_front(readLoop, &t, std::move(bufferHandler),
+                                 std::move(rdeCommandHandler)));
+    io.run();
 
-	return 0;
+    return 0;
 }
